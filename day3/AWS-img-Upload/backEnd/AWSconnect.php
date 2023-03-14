cd6abec78e731783ee1e6a2122ed45a7a106b08d
Mahmoud Mohamed <99666114+Mahmoud1499@users.noreply.github.com>
2023-03-14 15:36:33 +0200

    added with OOP

--- day3/AWS-img-Upload/backEnd/AWSconnect.php
+++ day3/AWS-img-Upload/backEnd/AWSconnect.php
@@ -4,7 +4,7 @@ use Aws\S3\S3Client;
 
 $s3 = S3Client::factory([
     'version' => 'latest',
-    'region' => 'us-east-1',
+    'region' => AWS_REGION,
     'credentials' => array(
         'key' => AWS_ACCESS_KEY_ID,
         'secret' => AWS_SERCRET_ACCESS_KEY,
